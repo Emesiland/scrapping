0dd31a6c597366bab9287e01bb93ca89c7b6fdb6
Emesiland <emesiland@mail.ru>
2025-08-31 18:42:55 +0400

    функция для парсинга

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from fake_headers import Headers
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 habr = 'https://habr.com/ru/articles/'
+
+def get_articles(url='https://habr.com/ru/articles'):
+    headers = Headers(headers=True).generate()
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles = []
