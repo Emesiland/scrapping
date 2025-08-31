5056adc97e221831f19d1716c59ff87144af4a08
Emesiland <emesiland@mail.ru>
2025-08-31 18:52:44 +0400

    фильтр

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from fake_headers import Headers
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-habr = 'https://habr.com/ru/articles/'
+habr = 'https://habr.com'
 
 def get_articles(url='https://habr.com/ru/articles'):
     headers = Headers(headers=True).generate()
@@ -17,4 +17,17 @@ def get_articles(url='https://habr.com/ru/articles'):
         a_tag = tag.find('h2').find('a')
         article_link = habr + a_tag.get('href')
         article_title = a_tag.find('span').text.strip()
+        
+        response = requests.get(article_link, headers=headers)
+        article_soup = BeautifulSoup(response.text, 'html.parser')
+        article_text = article_soup.find('div', id='post-content-body').text.strip()
+        
+        for keyword in KEYWORDS:
+            if keyword.lower() in article_title.lower() or keyword.lower() in article_text.lower():
+                articles.append(f"{publication_date} - {article_title} - {article_link}")
+                break
+    
+    return articles
 
+if __name__ == '__main__':
+    print(*get_articles(), sep='\n')
