90766e43d7409b5b3cbaee17c76847e886a51a0a
Emesiland <emesiland@mail.ru>
2025-08-31 18:48:07 +0400

    обработка статей

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ def get_articles(url='https://habr.com/ru/articles'):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
     articles = []
+
+    for tag in soup.find('div', class_='tm-articles-list').find_all('article'):
+        time_tag = tag.find('time')
+        publication_date = time_tag.get('datetime')[:10]
+        a_tag = tag.find('h2').find('a')
+        article_link = habr + a_tag.get('href')
+        article_title = a_tag.find('span').text.strip()
+
